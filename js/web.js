db04750dd6dd7196811240f77ab57d6eb64bcb97
hamburgerdog <838980000@qq.com>
2020-11-02 23:32:22 +0800

    作者页卡片特效更改,完善了个人简介

--- js/web.js
+++ js/web.js
@@ -1,20 +1,50 @@
 $(document).ready(function () {
     $("#card1").mouseenter(function () { 
         $(this).addClass("card_select");
+        $(this).css({
+            'width':'260px',
+            'height':'360px',
+            'top':'-=5px',
+            'left':'+=5px',
+            'margin-left':'-=10px'
+        })
         $("#card1 .msg_name").css({
             'color':'#94969a',
         });
         $("#card1 .msg_info").css({
             'color':'#3c3c3c',
         });
+        $("#card1 .msg_pic").css({
+            'left':'+=5px',
+            'top':'+=5px'
+        });
+        $("#card1 .msg_txt").css({
+            'left':'+=5px',
+            'top':'+=5px'
+        });
     });
     $("#card1").mouseleave(function () { 
         $(this).removeClass("card_select");
+        $(this).css({
+            'width':'250px',
+            'height':'350px',
+            'top':'+=5px',
+            'left':'-=5px',
+            'margin-left':'+=10px'
+        })
         $("#card1 .msg_name").css({
             'color':'#b2b4b6',
         });
         $("#card1 .msg_info").css({
             'color':'#94969a',
+        });
+        $("#card1 .msg_pic").css({
+            'left':'-=5px',
+            'top':'-=5px'
+        });
+        $("#card1 .msg_txt").css({
+            'left':'-=5px',
+            'top':'-=5px'
         });
     });
     $("#card2").mouseenter(function () { 
@@ -25,6 +55,21 @@ $(document).ready(function () {
         $("#card2 .msg_info").css({
             'color':'#3c3c3c',
         });
+        $(this).css({
+            'width':'260px',
+            'height':'360px',
+            'top':'-=5px',
+            'left':'+=5px',
+            'margin-left':'-=10px'
+        });
+        $("#card2 .msg_pic").css({
+            'left':'+=5px',
+            'top':'+=5px'
+        });
+        $("#card2 .msg_txt").css({
+            'left':'+=5px',
+            'top':'+=5px'
+        });
     });
     $("#card2").mouseleave(function () { 
         $(this).removeClass("card_select");
@@ -34,23 +79,74 @@ $(document).ready(function () {
         $("#card2 .msg_info").css({
             'color':'#94969a',
         });
+        $(this).css({
+            'width':'250px',
+            'height':'350px',
+            'top':'+=5px',
+            'left':'-=5px',
+            'margin-left':'+=10px'
+        });
+        $("#card2 .msg_pic").css({
+            'left':'-=5px',
+            'top':'-=5px'
+        });
+        $("#card2 .msg_txt").css({
+            'left':'-=5px',
+            'top':'-=5px'
+        });
     });
     $("#card3").mouseenter(function () { 
         $(this).addClass("card_select");
+        $(this).css({
+            'width':'260px',
+            'height':'360px',
+            'top':'-=5px',
+            'left':'+=5px',
+            'margin-left':'-=10px'
+        })
         $("#card3 .msg_name").css({
             'color':'#94969a',
         });
         $("#card3 .msg_info").css({
             'color':'#3c3c3c',
         });
+        $("#card3 .msg_pic").css({
+            'left':'+=5px',
+            'top':'+=5px'
+        });
+        $("#card3 .msg_txt").css({
+            'left':'+=5px',
+            'top':'+=5px'
+        });
     });
     $("#card3").mouseleave(function () { 
         $(this).removeClass("card_select");
+        $(this).css({
+            'width':'250px',
+            'height':'350px',
+            'top':'+=5px',
+            'left':'-=5px',
+            'margin-left':'+=10px'
+        })
         $("#card3 .msg_name").css({
             'color':'#b2b4b6',
         });
         $("#card3 .msg_info").css({
             'color':'#94969a',
         });
+        $("#card3 .msg_pic").css({
+            'left':'-=5px',
+            'top':'-=5px'
+        });
+        $("#card3 .msg_txt").css({
+            'left':'-=5px',
+            'top':'-=5px'
+        });
     });
+    // $("#card .msg_pic").click(function (e) { 
+    //     e.preventDefault();
+    //     $("").animate({
+    //         ''
+    //     })
+    // });
 });
